e7e13c30cc21ab73092ad42c2e6cd28f090fad77
Phumet27 <phumet.aun@ttbbank.com>
2025-09-14 05:08:20 +0700

    no message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // บังคับ static export
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/NK_battery_web',
+  assetPrefix: '/NK_battery_web/',
   images: {
     unoptimized: true, // ถ้าใช้ <Image> ให้ disable optimization
   },
